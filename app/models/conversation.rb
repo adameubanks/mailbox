7addb52ec4598cf4058ccab2033ba795411f9942
Adam <akhanrade@gmail.com>
2016-07-18 16:00:46 -0400

    Created Migrations for conversations and messages

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -6,7 +6,7 @@ class Conversation < ActiveRecord::Base
   validates_uniqueness_of :sender_id, :scope => :recipient_id
 
   scope :between, -> (sender_id, recipient_id) do
-    where ("conversations.sender_id = ? AND conversations.recipient_id = ?)
+    where ("(conversations.sender_id = ? AND conversations.recipient_id = ?)
     OR (conversations.sender_id = ? AND conversations.recipient_id = ?)",
     sender_id, recipient_id, recipient_id, sender_id)
   end
