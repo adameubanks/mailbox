a5f19cc679bc985ea5d8eefbf9a881424345bcc0
Adam <akhanrade@gmail.com>
2016-07-19 10:05:26 -0400

    Fixed Routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  #root 'welcome#index'
+  resources :conversations do
+    resources :messages
+  end
+  root 'conversations#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
